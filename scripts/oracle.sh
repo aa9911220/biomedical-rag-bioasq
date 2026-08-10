dc28b177f0f8a484dc67bccb626324d0c6a7ff48
Charlie's Github <aa9911220@gmail.com>
2026-08-10 11:24:50 +0200

    Add files via upload

---
+++ scripts/oracle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -A uppmax2026-1-123
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=02:00:00
+#SBATCH -J qwen_baseline
+#SBATCH -o results/baseline.out
+#SBATCH -e results/baseline.err
+
+cd /gorilla/home/yuli6752/medical
+
+source /home/yuli6752/miniconda3/etc/profile.d/conda.sh
+
+conda activate medrag
+
+export PYTHONNOUSERSITE=1
+
+echo "Python path:"
+which python
+
+echo "Torch version:"
+python -c "import torch; print(torch.__version__)"
+
+python src/oracle_rag.py
